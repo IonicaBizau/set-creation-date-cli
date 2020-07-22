d39437f96eb0246ba736c2c15e4e6cd15997fac9
Ionică Bizău <bizauionica@gmail.com>
2020-07-22 15:36:19 +0300

    :arrow_up: 1.0.0 :tada:

---
+++ DOCUMENTATION.md
@@ -0,0 +1,15 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `setCreationDateCli(a, b)`
+Change the creation date of multiple files to be their last modified time.
+
+#### Params
+
+- **Number** `a`: Param descrpition.
+- **Number** `b`: Param descrpition.
+
+#### Return
+- **Number** Return description.
+
---
+++ bin/set-creation-date-cli.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+"use strict";
+
+const Tilda = require("tilda")
+    , setCreationDateCli = require("..")
+    , util = require("util")
+    , glob = util.promisify(require("glob"))
+    , Utimes = require('@ronomon/utimes')
+    , changeUtimes = util.promisify(Utimes.utimes)
+    , fs = require("fs").promises
+    ;
+
+new Tilda(`${__dirname}/../package.json`, {
+    options: [
+        {
+            name: "verbose"
+          , opts: ["V", "verbose"]
+          , desc: "Enable verbose mode."
+          , type: Boolean
+        },
+        {
+            name: "extensions"
+          , opts: ["e", "extensions"]
+          , desc: "List the unique extensions of the files."
+          , type: Boolean
+        }
+    ]
+  , args: [{
+        name: "match"
+      , type: String
+      , desc: "The file match pattern."
+    }]
+}).main(async action => {
+
+    // Query the files
+    const files = await glob(action.args.match)
+
+    if (action.options.extensions.value) {
+        const uniqueExts = files.reduce((acc, c) => {
+            if (c.includes(".")) {
+                acc[c.split(".").slice(-1)[0]] = 1
+            }
+            return acc
+        }, {})
+        console.log(Object.keys(uniqueExts))
+        return
+    }
+
+    for (let i = 0; i < files.length; ++i) {
+        const cFile = files[i]
+        const stat = await fs.stat(cFile)
+        debugger
+        if (action.options.verbose.value) {
+            console.log(`Updating ${cFile}`, stat)
+        }
+        await changeUtimes(
+            cFile
+          , +stat.mtime
+          , +stat.mtime
+          , +stat.mtime
+        )
+    }
+});
